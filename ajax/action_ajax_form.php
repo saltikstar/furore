7fca6efba46565953eaab7beaffb713fd613bfa1
SaltikStar <saltik91@mail.ru>
2022-02-11 14:37:45 +0600

    reload project
    reload project

--- ajax/action_ajax_form.php
+++ ajax/action_ajax_form.php
@@ -7,7 +7,7 @@ ini_set('display_startup_errors', 1);
 $token = "5109024591:AAEAmCYMJAj7_vC24-RNKvxbi_3YaM2Qh2E";
 
 //Сюда вставляем chat_id
-$chat_id = "-792996520";
+$chat_id = "-1001689627084";
 
 //Определяем переменные для передачи данных из нашей формы
 if ($_POST["name"]!='' &&  $_POST["phone"]!='' ) { 
